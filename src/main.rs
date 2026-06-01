d38d6c6c0100707c8a166377df1a3d041fb8796f
Lucas Galante <lsgalante12@gmail.com>
2026-06-01 06:55:00 -0400

    Change wgpu power preference to LowPower to reduce resource usage

--- src/main.rs
+++ src/main.rs
@@ -272,7 +272,7 @@ impl ColorApp {
         });
         let wgpu_surface = instance.create_surface(wayland_handle).expect("surface");
         let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::HighPerformance,
+            power_preference: wgpu::PowerPreference::LowPower,
             compatible_surface: Some(&wgpu_surface),
             force_fallback_adapter: false,
         }).await.expect("adapter");
