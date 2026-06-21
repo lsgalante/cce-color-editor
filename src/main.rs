77a27a47e31a445cf37f98c55f2b5129fe728064
Lucas Galante <lsgalante12@gmail.com>
2026-06-21 09:20:37 -0400

    fix: use transparent clear_color to support rounded window corners

--- src/main.rs
+++ src/main.rs
@@ -744,7 +744,7 @@ impl Application for ColorApp {
     }
 
     fn clear_color(&self) -> [f32; 4] {
-        cce_ui::color::page_low_color()
+        [0.0, 0.0, 0.0, 0.0]
     }
 
     fn handle_pointer_move(&mut self, pos: LogicalPosition, needs_rebuild: &mut bool) {
